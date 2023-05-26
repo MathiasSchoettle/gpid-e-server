1ea003b10210cbb581b6686d1ac9fc550a1e167d
Mathias Schoettle <mathias.schoettle@st.othr.de>
2023-05-26 16:54:15 +0200

    db connection stuff

--- src/db.cpp
+++ src/db.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <postgresql/libpq-fe.h>
+#include "db.h"
 
 using namespace std;
 
@@ -11,4 +11,20 @@ void test_connection() {
 		puts("We were unable to connect to the database");
 		exit(0);
 	}
+}
+
+db::db() {
+
+}
+
+void db::save_device(std::string ip_address, std::string sys_description) {
+
+}
+
+std::vector<device> db::devices() {
+
+}
+
+void db::save_entry(std::string ip_address, float consumption) {
+	
 }
--- src/db.h
+++ src/db.h
@@ -1,3 +1,26 @@
 #pragma once
 
-void test_connection();
+#include <postgresql/libpq-fe.h>
+#include <string>
+#include <vector>
+
+struct device {
+	int id;
+	std::string ip_address;
+	std::string sys_description;
+};
+
+struct data_entry {
+	int id;
+	int timestamp;
+	int device_id;
+	float consumption;
+};
+
+class db {
+	db();
+	PGconn * connection;
+	void save_device(std::string ip_address, std::string sys_description);
+	std::vector<device> devices();
+	void save_entry(std::string ip_address, float consumption);
+};
